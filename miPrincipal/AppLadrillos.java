752e92b8c5c09a641d2fa196fc2d27c8212cb9d7
Sebastián Alcántar Pérez <112568851+Sebas8920@users.noreply.github.com>
2024-02-23 16:51:53 +0000

    NO HUBO PIZZITA :(

--- miPrincipal/AppLadrillos.java
+++ miPrincipal/AppLadrillos.java
@@ -1,7 +1,22 @@
 package miPrincipal;
+
 public class AppLadrillos{
 
     public static void menu(){
-        
+        System.out.println("Version iterativa");
+        dibujar_pared_ite(10);
+        System.out.println("Version recursiva");
+        dibujar_pared_rec(10);
     }
+        public static void dibujar_pared_ite(int n){
+            for(int i = 0; i < 10; i++){
+                System.out.println("xxxxxxxxxxxxxxxx");
+            }
+        }
+        public static void dibujar_pared_rec(int n){
+            if(n>0)
+                dibujar_pared_rec(n-1);
+                
+            System.out.println("xxxxxxxxxxxxxxxx");
+        }
 }
